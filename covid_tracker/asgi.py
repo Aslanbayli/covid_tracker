233394ab3d8a087050bd2efacbb464962a51085e
Aslanbayli <48028559+Aslanbayli@users.noreply.github.com>
2020-09-26 03:02:19 -0400

    hello

--- covid_tracker/asgi.py
+++ covid_tracker/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plogbook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covid_tracker.settings')
 
 application = get_asgi_application()
--- covid_tracker/wsgi.py
+++ covid_tracker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plogbook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covid_tracker.settings')
 
 application = get_wsgi_application()
