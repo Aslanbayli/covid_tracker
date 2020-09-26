f7d04797fc2dfeb7f60ef86ef7bd135c86214392
chloeb292 <66789673+chloeb292@users.noreply.github.com>
2020-09-26 02:16:23 -0400

    Test

---
+++ weekly_entry/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeeklyEntryConfig(AppConfig):
+    name = 'weekly_entry'
--- weekly_entry/models.py
+++ weekly_entry/models.py
@@ -10,9 +10,8 @@ from users.models import CustomUser
 class Student(models.Model):
     custom_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     
-    # Pilot Base Info
+    # Student Base Info
     first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200,blank=True)
     last_name = models.CharField(max_length=200)
 
 
@@ -21,23 +20,11 @@ class Student(models.Model):
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
+        ('X', 'Other')
     )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
-    # Contact
-    phone = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-
-    # Address
-
-    STATE_CHOICES = (("Alabama","Alabama"),("Alaska","Alaska"),("Arizona","Arizona"),("Arkansas","Arkansas"),("California","California"),("Colorado","Colorado"),("Connecticut","Connecticut"),("Delaware","Delaware"),("Florida","Florida"),("Georgia","Georgia"),("Hawaii","Hawaii"),("Idaho","Idaho"),("Illinois","Illinois"),("Indiana","Indiana"),("Iowa","Iowa"),("Kansas","Kansas"),("Kentucky","Kentucky"),("Louisiana","Louisiana"),("Maine","Maine"),("Maryland","Maryland"),("Massachusetts","Massachusetts"),("Michigan","Michigan"),("Minnesota","Minnesota"),("Mississippi","Mississippi"),("Missouri","Missouri"),("Montana","Montana"),("Nebraska","Nebraska"),("Nevada","Nevada"),("New Hampshire","New Hampshire"),("New Jersey","New Jersey"),("New Mexico","New Mexico"),("New York","New York"),("North Carolina","North Carolina"),("North Dakota","North Dakota"),("Ohio","Ohio"),("Oklahoma","Oklahoma"),("Oregon","Oregon"),("Pennsylvania","Pennsylvania"),("Rhode Island","Rhode Island"),("South Carolina","South Carolina"),("South Dakota","South Dakota"),("Tennessee","Tennessee"),("Texas","Texas"),("Utah","Utah"),("Vermont","Vermont"),("Virginia","Virginia"),("Washington","Washington"),("West Virginia","West Virginia"),("Wisconsin","Wisconsin"),("Wyoming","Wyoming"))
-
-    address1 = models.CharField(max_length=200)
-    address2 = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=200)
-    state = models.CharField(max_length=200, choices=STATE_CHOICES)
-    zipcode = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
+   
     
     def __str__(self): 
         return "%s %s" % (self.first_name , self.last_name)
