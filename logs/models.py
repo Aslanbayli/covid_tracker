187066e3104bce7a9ce7fc65b76f5f70c8b981c4
Aslanbayli <48028559+Aslanbayli@users.noreply.github.com>
2020-09-26 02:17:18 -0400

    test

--- logs/models.py
+++ logs/models.py
@@ -54,23 +54,13 @@ class WeeklyEntry(models.Model):
         ('6-15', '6-15 People'),
         ('16-20+', '16-20+ People'),
     )
-    DINING_HALL = (
-        ('In Dining Hall', 'Have Eaten in Dining Hall'),
-        ('Not In Dining Hall', 'Have Eaten in Room'),
-        ('Eaten Off Campus', 'Have Eaten Off Campus'),
-    )
-    LEFT_CAMPUS = (
-        ('Y', 'Have Left Campus'),
-        ('N', 'Have Not Left Campus'),
-    )
-    
 
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     current_symptoms = models.CharField(max_length=400, choices=SYMPTOM_CHOICES)
-    avg_people_seen = models.CharField(max_length=200, choices=AVG_PEOPLE)
-    dining_hall = models.CharField(max_length=200, choices=DINING_HALL)
-    leave_campus = models.CharField(max_length=200, choices=LEFT_CAMPUS)
+    # avg_people_seen = 
+    # dining_hall = 
+    # leave_campus = 
 
 
     def __str__(self): 
-        return "Current Symptoms: %s, Average People Seen in the Past Week: %s, Eating: %s, Have you left campus: %s " %(self.current_symptoms, self.avg_people_seen, self.dining_hall, self.leave_campus)
+        return "Current Symptoms: %s, Average People Seen in the Past Week: %s, Eating: %s, Have you left campus: %s " (self.current_symptoms, self.avg_people_seen, self.dining_hall, self.leave_campus)
